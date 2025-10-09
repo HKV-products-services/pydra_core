be2de99b24507b72d709876a59968d3788282e60
Niels van der Vegt <nielsvdv1996@gmail.com>
2025-10-09 15:14:43 +0200

    Update EFL

--- src/pydra_core/core/exceedance_frequency_line.py
+++ src/pydra_core/core/exceedance_frequency_line.py
@@ -17,7 +17,6 @@ class ExceedanceFrequencyLine(Calculation):
         self,
         result_variable: str,
         model_uncertainty: bool = True,
-        levels: list = None,
         step_size: float = 0.1,
     ):
         """
@@ -29,10 +28,8 @@ class ExceedanceFrequencyLine(Calculation):
             The result variable for which the frequency line will be calculated.
         model_uncertainty: bool
             Enable or disable the use of model uncertainties when calculating the frequency line. Default is True.
-        levels: list (optional):
-            The levels at which the exceedance probability has to be calculated. If not specified, the levels will be chosen between the 1st and 99th percentile of the values in the HRDatabase.
         step_size: float (optional)
-            The step size of the frequency line. Default is 0.1.
+            Step size of the frequency line discretisation. Default is 0.1.
         """
         # Inherit
         super().__init__()
@@ -40,9 +37,8 @@ class ExceedanceFrequencyLine(Calculation):
         # Save settings
         self.set_result_variable(result_variable.lower())
         self.use_model_uncertainty(model_uncertainty)
-        self.set_levels(levels)
-        self.set_step_size(step_size)
-        self.model_uncertainty_steps = None
+        self.set_step_size(0.1)
+        self.set_levels(None)
 
     def calculate_location(self, location: Location) -> FrequencyLine:
         """
@@ -59,52 +55,52 @@ class ExceedanceFrequencyLine(Calculation):
         FrequencyLine
             Frequency line of the result variable
         """
-        # Copy the levels
-        levels = self.levels
-
         # Obtain location object
         model = location.get_model()
         loading = model.get_loading()
         monz = model.get_statistics().get_model_uncertainties()
 
         # Check if the levels are defined, if not, define it between the 1st and 99th percentile
-        if levels is None:
+        if self.levels is None:
             lower, upper = loading.get_quantile_range(self.result_variable, 0.0, 1.0, 3)
             levels = np.arange(lower, upper + 0.5 * self.step_size, self.step_size)
+        else:
+            levels = self.levels
 
         # Model uncertainty
         if self.model_uncertainty:
+
             # Model uncertainty steps (if None use default)
-            if self.model_uncertainty_steps is None:
-                self.model_uncertainty_steps = monz.step_size[self.result_variable]
+            model_uncertainty_steps = monz.step_size[self.result_variable]
 
             # Discretise
-            _, edges = monz.model_uncertainties[1, self.result_variable].discretise(self.model_uncertainty_steps)
+            _, edges = monz.model_uncertainties[1, self.result_variable].discretise(model_uncertainty_steps)
             p = np.diff(norm.cdf(edges))
 
         # If not
         else:
-            self.model_uncertainty_steps = 1
+            model_uncertainty_steps = 1
             p = [1.0]
 
         # Discretise
         exp = 0
         for _ip, _p in enumerate(p):
+
             # Deepcopy
             _model = deepcopy(model)
             _loading = _model.get_loading()
+
+            # Repair
+            _loading.repair_loadingmodels(self.result_variable)
 
             # Adjust loading models
             if self.model_uncertainty:
                 for deelmodel, result in _loading.model.items():
                     _unc = monz.model_uncertainties[deelmodel[1], self.result_variable]
-                    _disc, _ = _unc.discretise(self.model_uncertainty_steps)
+                    _disc, _ = _unc.discretise(model_uncertainty_steps)
                     _data = getattr(result, self.result_variable)
                     _data = _data + _disc[_ip] if self.result_variable == "h" else _data * _disc[_ip]
                     setattr(result, self.result_variable, _data)
-
-            # Repair
-            _loading.repair_loadingmodels(self.result_variable)
 
             # Splits uit naar trage stochasten en windrichting
             p_h_slow = _model.calculate_probability_loading(
@@ -152,6 +148,17 @@ class ExceedanceFrequencyLine(Calculation):
         # Save result variable
         self.result_variable = result_variable
 
+    def use_model_uncertainty(self, model_uncertainty: bool):
+        """
+        Use model uncertainty when calculating a frequency line.
+
+        Parameters
+        ----------
+        model_uncertainty : bool
+            Enable or disable the use of model uncertainties
+        """
+        self.model_uncertainty = model_uncertainty
+
     def set_levels(self, levels: list = None):
         """
         Change the levels.
@@ -179,25 +186,3 @@ class ExceedanceFrequencyLine(Calculation):
 
         # Save step size
         self.step_size = step_size
-
-    def use_model_uncertainty(self, model_uncertainty: bool):
-        """
-        Use model uncertainty when calculating a frequency line.
-
-        Parameters
-        ----------
-        model_uncertainty : bool
-            Enable or disable the use of model uncertainties
-        """
-        self.model_uncertainty = model_uncertainty
-
-    def set_model_uncertainty_steps(self, model_uncertainty_steps: int):
-        """
-        Set the number of model uncertainty steps
-
-        Parameters
-        ----------
-        model_uncertainty_steps : int
-            Number of model uncertainty steps
-        """
-        self.model_uncertainty_steps = model_uncertainty_steps
